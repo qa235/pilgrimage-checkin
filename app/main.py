71b10cd627c3590aaf22fabbbf60bc8e3721a4e9
qa235 <qakaty672@gamil.com>
2026-03-05 16:49:23 +0800

    fix homepage map

--- app/main.py
+++ app/main.py
@@ -5,28 +5,28 @@ from pydantic import BaseModel
 import sqlite3
 import pathlib
 from datetime import datetime
-
+from fastapi.responses import FileResponse
 
 
 app = FastAPI()
+
 os.makedirs("uploads/photos", exist_ok=True)
 
-#建立資料庫
+# 建立資料庫
 conn = sqlite3.connect("checkins.db", check_same_thread=False)
 cursor = conn.cursor()
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS checkins (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     lat REAL,
     lng REAL,
     time TEXT,
-    photo TEXT           
-               
+    photo TEXT
 )
 """)
-conn.commit()
 
-# 儲存打卡資料
+conn.commit()
 
 
 class Checkin(BaseModel):
@@ -34,13 +34,16 @@ class Checkin(BaseModel):
     lng: float
 
 
+# 首頁顯示地圖
 @app.get("/")
 def home():
-    return {"message": "Pilgrimage Check-in API is running"}
+    return FileResponse("static/index.html")
+
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
 
 @app.post("/checkin")
 def add_checkin(data: Checkin):
@@ -49,7 +52,7 @@ def add_checkin(data: Checkin):
 
     cursor.execute(
         "INSERT INTO checkins (lat,lng,time) VALUES (?,?,?)",
-        (data.lat,data.lng,now)
+        (data.lat, data.lng, now)
     )
 
     conn.commit()
@@ -61,43 +64,47 @@ def add_checkin(data: Checkin):
     }
 
 
-
 @app.post("/checkin_photo")
 async def checkin_photo(
-    lat:float = Form(...),
-    lng:float= Form(...),
-    file:UploadFile =File(...)
+    lat: float = Form(...),
+    lng: float = Form(...),
+    file: UploadFile = File(...)
 ):
-    now =datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     filename = pathlib.Path(file.filename).name
     filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{filename}"
+
     filepath = os.path.join("uploads/photos", filename)
 
-    with open(filepath,"wb")as buffer:
+    with open(filepath, "wb") as buffer:
         buffer.write(await file.read())
 
     cursor.execute(
         "INSERT INTO checkins(lat,lng,time,photo) VALUES(?,?,?,?)",
-        (lat,lng,now,filename)
+        (lat, lng, now, filename)
     )
 
     conn.commit()
-    return{
-        "status":"success",
-        "photo":filename
-    }    
 
-# 新增這段
+    return {
+        "status": "success",
+        "photo": filename
+    }
+
+
 @app.get("/checkins")
 def get_checkins():
 
     cursor.execute("SELECT lat,lng,time,photo FROM checkins")
     rows = cursor.fetchall()
-    
+
     results = [
-    {"lat": r[0], "lng": r[1], "time": r[2], "photo": r[3]}
-    for r in rows
-]
+        {"lat": r[0], "lng": r[1], "time": r[2], "photo": r[3]}
+        for r in rows
+    ]
+
     return results
 
 
